22c41b727bcb471ff6acf3cca8c25cbb84d5bf90
naoyasakua <4869ns@gmail.com>
2020-12-07 16:13:44 +0900

    コメント投稿機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog) }
+      else
+        format.html { redirect_to blog_path(@blog), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:blog_id, :content)
   end
 end
